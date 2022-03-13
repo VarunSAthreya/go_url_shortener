b5ab8f5f11dc9bdcbbc33029df36d88b5fe6a415
Varun S Athreya <varun.sathreya@gmail.com>
2022-03-13 17:31:12 +0530

    Initialized project

---
+++ api/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/VarunSAthreya/go-url-shortner/routes"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
+)
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/:url", routes.ResolveURL)
+
+	app.Post("/api/v1", routes.ShortenURL)
+}
+
+func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	app := fiber.New()
+	app.Use(logger.New())
+
+	setupRoutes(app)
+
+	log.Fatal(app.Listen(os.Getenv("DOMAIN")))
+
+}
